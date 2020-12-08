0ef59a2653c945c9fb04bf40720f2c64832bf853
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2020-12-08 23:11:28 +0200

    добавляю лейзисайзес с блюром

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,17 +2,24 @@ import React from 'react';
 import './ImageGalleryItem.css';
 import authContext from '../Context';
 import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 
-export default function ImageGalleryItem({ id, webformatURL, largeImageURL }) {
+export default function ImageGalleryItem({
+  id,
+  webformatURL,
+  largeImageURL,
+  previewURL,
+}) {
   return (
     <li className="ImageGalleryItem" key={`id-${id}`}>
       <authContext.Consumer>
         {hadleImageClick => (
           <img
+            src={previewURL}
             data-src={webformatURL}
             alt="searchedPicture"
             data-large={largeImageURL}
-            className="ImageGalleryItem-image lazyload"
+            className="ImageGalleryItem-image lazyload blur-up"
             onClick={hadleImageClick}
           />
         )}
